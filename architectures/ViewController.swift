3d66ec02b3f2086cc40b031229ff6139d28f2f3a
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-05-19 14:09:15 +0900

    Initial Commit

---
+++ architectures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  architectures
+//
+//  Created by YutoMizutani on 2018/05/19.
+//  Copyright © 2018 Yuto Mizutani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

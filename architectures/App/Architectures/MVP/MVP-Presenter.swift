278d30358454fdb14fa0f5c68f41f1718de93429
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-05-26 22:46:49 +0900

    :sparkles: Add MVP pattern

--- architectures/App/Architectures/MVP/MVP-Presenter.swift
+++ architectures/App/Architectures/MVP/MVP-Presenter.swift
@@ -16,13 +16,11 @@
  */
 
 import UIKit
-import RxSwift
-import RxCocoa
-import Eureka
 
 class MVPPresenter: UIViewController {
-    var subview: MVPView = MVPView()
-    var model: MVPModel? = nil
+    // ViewとModelを保持する。
+    var subview: MVPView!
+    var model: MVPModel!
 }
 
 extension MVPPresenter {
@@ -30,9 +28,16 @@ extension MVPPresenter {
         super.viewDidLoad()
 
         configureView()
+        configureModel()
         layoutView()
         addAction()
-        setFirstValue()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // 残高を取得する。
+        self.fetchValue()
     }
 
     override func viewDidLayoutSubviews() {
@@ -50,49 +55,87 @@ extension MVPPresenter {
     }
 }
 
+// MARK:- Private methods about settings
 extension MVPPresenter {
+    /// Viewの構成を行う。
     private func configureView() {
+        // Viewを作成する。
+        self.subview = MVPView()
+
+        // Viewに表示されるラベルの名前を設定する。
+        self.subview.toView.nameLabel.text = "\(UserList.takahashi.rawValue): "
+        self.subview.fromView.nameLabel.text = "\(UserList.watanabe.rawValue): "
+
+        // Viewに追加する。
         self.view.addSubview(self.subview)
     }
 
+    /// Viewの更新を行う。
     private func layoutView() {
+        // subviewのサイズを更新する。
         self.subview.frame = self.view.frame
     }
 
-    private func addAction() {
-//        self.subview.transferButton.addTarget(self, action: #selector(self.transfer), for: .touchUpInside)
-//        self.subview.resetButton.addTarget(self, action: #selector(self.resetCount), for: .touchUpInside)
+    /// Modelの構成を行う。
+    private func configureModel() {
+        // 使用するUserの定義
+        let users: [UserList] = [.takahashi, .watanabe]
+
+        // UserLisetを元にModelを作成する。
+        self.model = MVPModel(users: users)
     }
 
-    private func setFirstValue() {
-        let accounts = ""
+    /// ボタン押下時のアクションを設定する。
+    private func addAction() {
+        self.subview.transferButton.addTarget(self, action: #selector(self.transfer), for: .touchUpInside)
+        self.subview.resetButton.addTarget(self, action: #selector(self.resetCount), for: .touchUpInside)
+    }
+}
 
+extension MVPPresenter {
+    private func fetchValue() {
+        // Model要素を更新する。
+        self.model.fetch()
+
+        // Viewを更新する。
+        self.updateView()
+    }
+
+    private func updateView() {
+        // メインスレッドでラベルの値を更新する。
         DispatchQueue.main.async {
-//            self.subview..text = "0"
+            self.subview.toView.valueLabel.text = "\(self.model.accounts[UserList.takahashi.rawValue] ?? 0)"
+            self.subview.fromView.valueLabel.text = "\(self.model.accounts[UserList.watanabe.rawValue] ?? 0)"
         }
     }
 }
 
-extension MVPPresenter {
-    private func fetch() {
+extension MVPPresenter: ErrorShowable {
+    @IBAction func transfer() {
+        do {
 
-    }
-}
+            // 送金処理を行う。
+            try self.model.transfer(from: .watanabe, to: .takahashi, amount: Assets.amount)
 
-extension MVPPresenter {
-    @IBAction func countUp() {
-//        let text = "\(self.model.countUp())"
-//
-//        DispatchQueue.main.async {
-//            self.subview.displayLabel.text = text
-//        }
+            // Viewを更新する。
+            self.updateView()
+
+        }catch let e {
+
+            // エラーが生じた場合はアラートを表示する。
+            self.showAlert(error: e)
+
+        }
     }
 
     @IBAction func resetCount() {
-//        let text = "\(self.model.resetCount())"
-//
-//        DispatchQueue.main.async {
-//            self.subview.displayLabel.text = text
-//        }
+        // 使用するUserの定義
+        let users: [UserList] = [.takahashi, .watanabe]
+
+        // Userを元にリセットを行う。
+        self.model.reset(users)
+
+        // Viewを更新する。
+        self.updateView()
     }
 }

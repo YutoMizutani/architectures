705c1d615b778a8f3719602751590ea6b80ff426
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-06-24 22:08:54 +0900

    :art: Change ViewController written places from Presenter to View

--- architectures/App/Architectures/MVP/MVP-View.swift
+++ architectures/App/Architectures/MVP/MVP-View.swift
@@ -15,4 +15,132 @@
 
  */
 
-class MVPView: View {}
+import UIKit
+
+class MVPView: UIViewController {
+    // ViewとModelを保持する。
+    var subview: View!
+    var model: MVPModel!
+    var presenter: MVPPresenter!
+}
+
+extension MVPView {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureView()
+        configureModel()
+        configurePresenter()
+        layoutView()
+        addAction()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // 残高を取得する。
+        self.fetchValue()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        layoutView()
+
+        self.view.layoutIfNeeded()
+    }
+
+    override func viewSafeAreaInsetsDidChange() {
+        super.viewSafeAreaInsetsDidChange()
+
+        layoutView()
+    }
+}
+
+// MARK:- Private methods about settings
+extension MVPView {
+    /// Viewの構成を行う。
+    private func configureView() {
+        // Viewを作成する。
+        self.subview = View()
+
+        // Viewに表示されるラベルの名前を設定する。
+        self.subview.toView.nameLabel.text = "\(UserList.takahashi.rawValue): "
+        self.subview.fromView.nameLabel.text = "\(UserList.watanabe.rawValue): "
+
+        // Viewに追加する。
+        self.view.addSubview(self.subview)
+    }
+
+    /// Viewの更新を行う。
+    private func layoutView() {
+        // subviewのサイズを更新する。
+        self.subview.frame = self.view.frame
+    }
+
+    /// Modelの構成を行う。
+    private func configureModel() {
+        // 使用するUserの定義
+        let users: [UserList] = [.takahashi, .watanabe]
+
+        // UserLisetを元にModelを作成する。
+        self.model = MVPModel(users: users)
+    }
+
+    // Presenterの構成を行う。
+    private func configurePresenter() {
+        // Presenterを作成する。
+        self.presenter = MVPPresenter()
+    }
+
+    /// ボタン押下時のアクションを設定する。
+    private func addAction() {
+        self.subview.transferButton.addTarget(self, action: #selector(self.transfer), for: .touchUpInside)
+        self.subview.resetButton.addTarget(self, action: #selector(self.resetCount), for: .touchUpInside)
+    }
+}
+
+extension MVPView {
+    private func fetchValue() {
+        // Model要素を更新する。
+        self.model.fetch()
+
+        // Viewを更新する。
+        self.updateView()
+    }
+
+    private func updateView() {
+        // メインスレッドでラベルの値を更新する。
+        DispatchQueue.main.async {
+            self.subview.toView.valueLabel.text = "\(self.model.accounts[UserList.takahashi.rawValue] ?? 0)"
+            self.subview.fromView.valueLabel.text = "\(self.model.accounts[UserList.watanabe.rawValue] ?? 0)"
+        }
+    }
+}
+
+extension MVPView: ErrorShowable {
+    @IBAction func transfer() {
+        do {
+
+            // 送金処理を行う。
+            try self.presenter.transfer(self.model)
+
+            // Viewを更新する。
+            self.updateView()
+
+        }catch let e {
+
+            // エラーが生じた場合はアラートを表示する。
+            self.showAlert(error: e)
+
+        }
+    }
+
+    @IBAction func resetCount() {
+        // Userを元にリセットを行う。
+        self.presenter.reset(self.model)
+
+        // Viewを更新する。
+        self.updateView()
+    }
+}

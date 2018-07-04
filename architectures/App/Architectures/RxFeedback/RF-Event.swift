25f0696cbcef701af29238c9725f6ec80bbc74f4
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-07-04 14:33:01 +0900

    :scissors: Remove outed comment

--- architectures/App/Architectures/RxFeedback/RF-Event.swift
+++ architectures/App/Architectures/RxFeedback/RF-Event.swift
@@ -15,23 +15,3 @@ public enum RFEvent {
     case reset
     case transfer(to: UserList, from: UserList, amount: Int)
 }
-//
-//public extension RFEvent {
-//    /// textと一致するEventを返す。
-//    public static func throwState(_ text: String) -> RFEvent {
-//        switch text.lowercased() {
-//        case "goStart".lowercased(), "start".lowercased(), "s":
-//            return .goStart
-//        case "goSleep".lowercased(), "sleep".lowercased():
-//            return .goSleep
-//        case "goWakeUp".lowercased(), "wakeUp".lowercased():
-//            return .goWakeUp
-//        case "goEnd".lowercased(), "end".lowercased():
-//            return .goEnd
-//        case "response".lowercased(), "r":
-//            return .response
-//        default:
-//            return .noChange
-//        }
-//    }
-//}
